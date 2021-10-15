bdec1452a239967ecff35a6e755cee567a607d87
Martin Burolla <mburolla@xpanxion.com>
2021-10-15 10:16:13 -0400

    update

--- src/main/java/ProgramControl/Main.java
+++ src/main/java/ProgramControl/Main.java
@@ -2,6 +2,9 @@ package ProgramControl;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Worker worker = new Worker();
+        worker.varaibles();
+        worker.functions();
+        worker.loops();
     }
 }
---
+++ src/main/java/ProgramControl/Worker.java
@@ -0,0 +1,18 @@
+package ProgramControl;
+
+public class Worker {
+
+    public Worker() { } // Constructor
+
+    public void varaibles() {
+        System.out.println("variables");
+    }
+
+    public void loops() {
+        System.out.println("loops");
+    }
+
+    public void functions() {
+        System.out.println("functions");
+    }
+}
