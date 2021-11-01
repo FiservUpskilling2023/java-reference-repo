0878717870eb880adbbaf8da82eefd4d746464ec
Martin Burolla <mburolla@xpanxion.com>
2021-11-01 13:36:10 -0400

    update

--- src/main/java/ProgramControl/Main.java
+++ src/main/java/ProgramControl/Main.java
@@ -3,15 +3,15 @@ package ProgramControl;
 public class Main {
     public static void main(String[] args) {
         Worker worker = new Worker();
-//        worker.variables();
-//        worker.scope();
-//        worker.ifElse();
-//        worker.switchKeyword();
-//        worker.loops();
-//        worker.equalityTest();
-//        worker.functions();
-//        worker.processInput();
-//        worker.optionals();
+        worker.variables();
+        worker.scope();
+        worker.ifElse();
+        worker.switchKeyword();
+        worker.loops();
+        worker.equalityTest();
+        worker.functions();
+        worker.processInput();
+        worker.optionals();
         worker.enums();
     }
 }
