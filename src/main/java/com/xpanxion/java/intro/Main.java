b67f7e39325c508bf8f8647467484f13776aefb5
Martin Burolla <mburolla@xpanxion.com>
2021-12-01 11:23:13 -0500

    tidy

--- src/main/java/com/xpanxion/java/intro/Main.java
+++ src/main/java/com/xpanxion/java/intro/Main.java
@@ -4,17 +4,17 @@ public class Main {
     public static void main(String[] args) {
         Worker worker = new Worker();
         worker.variables();
-        worker.scope();
-        worker.ifElse();
-        worker.switchKeyword();
-        worker.loops();
-        worker.equalityVersusIdentityTest();
-        worker.functions();
-        worker.processInput();
-        worker.optionals();
-        worker.enums();
-        worker.constants();
-        worker.casting();
-        worker.precendence();
+//        worker.scope();
+//        worker.ifElse();
+//        worker.switchKeyword();
+//        worker.loops();
+//        worker.equalityVersusIdentityTest();
+//        worker.functions();
+//        worker.processInput();
+//        worker.optionals();
+//        worker.enums();
+//        worker.constants();
+//        worker.casting();
+//        worker.precendence();
     }
 }
